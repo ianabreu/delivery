69530235db020d6c043c3cc73e1c71958dde42f6
ianabreu <ianabreudev@gmail.com>
2023-12-08 05:21:25 -0300

    feat: add Sidebar component

---
+++ src/components/layout/sidebar.tsx
@@ -0,0 +1,42 @@
+import { Button } from "../ui/button";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "../ui/sheet";
+import { ReactNode } from "react";
+import { MENU_ICON } from "@/constants/Icons";
+
+interface SidebarProps {
+  children?: ReactNode;
+  side?: "left" | "right" | "bottom" | "top";
+  icon?: keyof typeof MENU_ICON;
+  header?: ReactNode;
+}
+
+const Sidebar = ({
+  children,
+  side = "left",
+  icon = "drawer",
+  header,
+}: SidebarProps) => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button
+          size="icon"
+          variant={"default"}
+          className="hover:bg-destructive"
+        >
+          {MENU_ICON[icon]}
+        </Button>
+      </SheetTrigger>
+      <SheetContent side={side} className="flex h-full w-[90%] flex-col gap-2">
+        {header && (
+          <SheetHeader className="text-left text-lg font-bold">
+            {header}
+          </SheetHeader>
+        )}
+        {children}
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default Sidebar;
