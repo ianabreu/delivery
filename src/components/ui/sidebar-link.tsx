2342b9cefe9a8c3e476dfdfde78100bd91e72c45
ianabreu <ianabreudev@gmail.com>
2023-12-08 05:21:11 -0300

    feat: add SidebarLink component

---
+++ src/components/ui/sidebar-link.tsx
@@ -0,0 +1,36 @@
+import { ReactNode } from "react";
+import { Button } from "../ui/button";
+import { SheetClose } from "../ui/sheet";
+import Link from "next/link";
+import { SIDEBAR_ICON } from "@/constants/Icons";
+
+interface SidebarLinkProps {
+  children: ReactNode;
+  icon?: keyof typeof SIDEBAR_ICON;
+  href?: string;
+  onClick?: () => void;
+}
+
+const SidebarLink = ({
+  children,
+  icon,
+  href = "/",
+  onClick,
+}: SidebarLinkProps) => {
+  return (
+    <SheetClose asChild>
+      <Link href={href}>
+        <Button
+          variant="ghost"
+          className="w-full justify-start gap-2"
+          onClick={onClick}
+        >
+          {icon && SIDEBAR_ICON[icon]}
+          {children}
+        </Button>
+      </Link>
+    </SheetClose>
+  );
+};
+
+export default SidebarLink;
