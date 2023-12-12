15152377dc0a2f5dfdff9450de68b35df8ab62e5
ianabreu <ianabreudev@gmail.com>
2023-12-12 05:00:01 -0300

    feat: add Footer component

---
+++ src/components/layout/footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="flex flex-col items-center justify-center h-10">
+      <p className="text-xs text-gray-500">Pede Fácil</p>
+      <p className="text-xs text-gray-500">Todos os direitos reservados</p>
+    </footer>
+  );
+};
+
+export default Footer;
